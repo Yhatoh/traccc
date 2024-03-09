64ffed5a2b24c39b6c279b16374043391c6653e1
uwo-o <hugo.campos@usm.cl>
2024-03-09 02:51:14 +0100

    formated

--- device/kokkos/src/seeding/spacepoint_binning.cpp
+++ device/kokkos/src/seeding/spacepoint_binning.cpp
@@ -33,6 +33,10 @@ spacepoint_binning::output_type spacepoint_binning::operator()(
     // Get the spacepoint sizes from the view
     auto sp_size = m_copy.get_size(spacepoints_view);
 
+    if (sp_size == 0) {
+        return {m_axes.first, m_axes.second, {}, m_mr.main, m_mr.host};
+    }
+
     // Set up the container that will be filled with the required capacities for
     // the spacepoint grid.
     const std::size_t grid_bins = m_axes.first.n_bins * m_axes.second.n_bins;
@@ -42,11 +46,6 @@ spacepoint_binning::output_type spacepoint_binning::operator()(
     m_copy.memset(grid_capacities_buff, 0);
     vecmem::data::vector_view<unsigned int> grid_capacities_view =
         grid_capacities_buff;
-
-    // Calculate the number of threads and thread blocks to run the kernels for.
-    // const unsigned int num_threads = 32 * 8;
-    // const unsigned int num_blocks = (sp_size + num_threads - 1) /
-    // num_threads;
 
     Kokkos::parallel_for(
         "count_grid_capacities", sp_size, KOKKOS_LAMBDA(const uint64_t i) {
@@ -66,6 +65,7 @@ spacepoint_binning::output_type spacepoint_binning::operator()(
         std::vector<std::size_t>(grid_capacities_host.begin(),
                                  grid_capacities_host.end()),
         m_mr.main, m_mr.host, vecmem::data::buffer_type::resizable);
+
     m_copy.setup(grid_buffer._buffer);
     sp_grid_view grid_view = grid_buffer;
 
@@ -74,7 +74,7 @@ spacepoint_binning::output_type spacepoint_binning::operator()(
         "populate_grid", sp_size, KOKKOS_LAMBDA(const uint64_t i) {
             device::populate_grid(i, m_config, spacepoints_view, grid_view);
         });
-
+    
     // Return the freshly filled buffer.
     return grid_buffer;
 }
